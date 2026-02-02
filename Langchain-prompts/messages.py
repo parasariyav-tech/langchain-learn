99c012e7b927efcadf011488a490e76cbe820d02
virender-parasariya <parasariyav@gmail.com>
2026-02-02 20:53:53 +0530

    adding files 2

--- Langchain-prompts/messages.py
+++ Langchain-prompts/messages.py
@@ -1,22 +1,18 @@
-from langchain_openai import ChatOpenAI
 from langchain_core.messages import SystemMessage, HumanMessage, AIMessage
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
 
 model = ChatOpenAI()
 
-chat_history = [
-    SystemMessage(content='You are a helpful AI assistant')
+messages=[
+    SystemMessage(content='You are a helpful assistant'),
+    HumanMessage(content='Tell me about LangChain')
 ]
 
-while True:
-    user_input = input('You: ')
-    chat_history.append(HumanMessage(content=user_input))
-    if user_input == 'exit':
-        break
-    result = model.invoke(chat_history)
-    chat_history.append(AIMessage(content=result.content))
-    print("AI: ",result.content)
+result = model.invoke(messages)
 
-print(chat_history)
+messages.append(AIMessage(content=result.content))
+
+print(messages)
