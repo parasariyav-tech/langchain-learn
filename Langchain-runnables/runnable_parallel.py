0b74fcc9657e169dc8369965350a612f84032dc0
virender-parasariya <parasariyav@gmail.com>
2026-02-06 00:01:55 +0530

    finishing runnables

--- Langchain-runnables/runnable_parallel.py
+++ Langchain-runnables/runnable_parallel.py
@@ -2,17 +2,17 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence, RunnableParallel
+from langchain_core.runnables import RunnableSequence, RunnableParallel
 
 load_dotenv()
 
 prompt1 = PromptTemplate(
-    template='Generate a tweet about {topic}',
+    template='Generate a tweet about {topic} following tweet word limit',
     input_variables=['topic']
 )
 
 prompt2 = PromptTemplate(
-    template='Generate a Linkedin post about {topic}',
+    template='Generate a Linkedin post about {topic} in 100 words',
     input_variables=['topic']
 )
 
@@ -25,7 +25,7 @@ parallel_chain = RunnableParallel({
     'linkedin': RunnableSequence(prompt2, model, parser)
 })
 
-result = parallel_chain.invoke({'topic':'AI'})
+result = parallel_chain.invoke({'topic':'KarmaFarming'})
 
 print(result['tweet'])
 print(result['linkedin'])
--- Langchain-runnables/runnable_passthrough.py
+++ Langchain-runnables/runnable_passthrough.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain.schema.runnable import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
+from langchain_core.runnables import RunnableSequence, RunnableLambda, RunnablePassthrough, RunnableParallel
 
 load_dotenv()
 
@@ -27,7 +27,7 @@ parallel_chain = RunnableParallel({
 
 final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
 
-result = final_chain.invoke({'topic':'AI'})
+result = final_chain.invoke({'topic':'One Sided Love'})
 
 final_result = """{} \n word count - {}""".format(result['joke'], result['word_count'])
 
